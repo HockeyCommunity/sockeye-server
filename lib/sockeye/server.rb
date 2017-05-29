65a7d43fda9853a61fe98e038b9bf1264170fd65
Jack Hayter <jack.hayter@googlemail.com>
2017-05-28 18:56:53 -0700

    Added a bunch of debug output for a project

--- lib/sockeye/server.rb
+++ lib/sockeye/server.rb
@@ -14,6 +14,7 @@ module Sockeye
       self.port = port
       self.secret_token = secret_token
       self.authentication_method = authentication_method
+      puts "welcome"
     end
 
     # Safely parse data as JSON, but return nil values on failure
@@ -29,13 +30,19 @@ module Sockeye
     # Call the supplied authentication method
     #
     def authenticate(token)
-      return self.authentication_method.call(token)
+      puts "authenticating:"
+      puts token
+      authenticated = self.authentication_method.call(token)
+      puts authenticated
+      return authenticated
     end
 
     # Add a connection to the list and add a map entry to link the
     # connection object with an authenticated identifier
     #
     def add_connection(identifier:, connection:)
+      puts "adding connection: "
+      puts identifier
       connections[identifier] = [] if connections[identifier].nil?
       connections[identifier] << connection
       connection_map[connection.object_id] = identifier
@@ -45,6 +52,8 @@ module Sockeye
     #
     def remove_connection(connection)
       identifier = connection_map[connection.object_id]
+      puts "removing connection:"
+      puts identifier
       if connections[identifier].is_a? Array
         connections[identifier].delete(connection)
         connections.delete(identifier) if connections[identifier].empty?
@@ -56,11 +65,21 @@ module Sockeye
     # then attempt to push the payload to each of them
     #
     def deliver_to_many(payload:, identifiers:)
+      puts "delivering payload:"
+      puts payload.inspect
+      puts "to identifiers:"
+      puts identifiers.inspect
       identifiers.each do |identifier|
+        puts "identifier:"
+        puts identifier
         identified_connections = connections[identifier]
+        puts "found #{identified_connections&.count.to_i} connections"
         next unless identified_connections.is_a? Array
         identified_connections.each do |connection|
+          puts "connection:"
+          puts connection.inspect
           begin
+            puts "sending payload"
             connection.send({payload: payload, status: payload.dig(:status) || 200}.to_json, :type => :text)
           rescue
           end
@@ -76,10 +95,12 @@ module Sockeye
       EM.run do
         WebSocket::EventMachine::Server.start(host: self.host, port: self.port) do |ws|
 
+          puts "listening"
+
           # Called when a new message arrives at the server
           #
           ws.onmessage do |message, type|
-
+            puts "message received"
             # Attempt to parse the received data as JSON
             #
             message_json = json_try_parse(message)
@@ -96,6 +117,7 @@ module Sockeye
               # method supplied on server setup
               #
               when :authenticate
+                puts "authentication reuqest"
                 authentication_result = authenticate(message_json[:payload])
                 if authentication_result
                   add_connection(identifier: authentication_result, connection: ws)
@@ -109,16 +131,19 @@ module Sockeye
               # then push out the payload to all connected specified clients
               #
               when :deliver
+                puts "delivery request"
                 if message_json[:secret_token] == self.secret_token
                   deliver_to_many(payload: message_json[:payload], identifiers: [message_json[:identifiers]].flatten)
                   ws.send({payload: "payload pushed", status: 201}.to_json, :type => :text)
                   ws.close
                 else
+                  puts "delivery auth failure"
                   ws.send({payload: "authentication failure", status: 401}.to_json, :type => :text)
                   ws.close
                 end
 
               else
+                puts "invalid request"
                 ws.send({payload: "invalid action", status: 405}.to_json, :type => :text)
                 ws.close
               end
