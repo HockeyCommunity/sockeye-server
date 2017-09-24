6ca5238ec04c01fb27be6576175a1373a27eee2e
Jack Hayter <jack@hockey-community.com>
2017-09-23 20:24:01 -0700

    Added debug logs

--- lib/sockeye/server.rb
+++ lib/sockeye/server.rb
@@ -19,6 +19,7 @@ module Sockeye
     # Safely parse data as JSON, but return nil values on failure
     #
     def json_try_parse(data)
+      puts "json_try_parse"
       begin
         return JSON.parse(data, symbolize_names: true)
       rescue JSON::ParserError => e
@@ -29,6 +30,7 @@ module Sockeye
     # Call the supplied authentication method
     #
     def authenticate(token)
+      puts "authenticate"
       return self.authentication_method.call(token)
     end
 
@@ -36,6 +38,9 @@ module Sockeye
     # connection object with an authenticated identifier
     #
     def add_connection(identifier:, connection:)
+      puts "add_connection"
+      puts "identifier"
+      puts identifier
       connections[identifier] = [] if connections[identifier].nil?
       connections[identifier] << connection
       connection_map[connection.object_id] = identifier
@@ -44,7 +49,10 @@ module Sockeye
     # Safely remove the specified connection from the connections lists
     #
     def remove_connection(connection)
+      puts "remove_connection"
       identifier = connection_map[connection.object_id]
+      puts "identifier:"
+      puts identifier
       if connections[identifier].is_a? Array
         connections[identifier].delete(connection)
         connections.delete(identifier) if connections[identifier].empty?
@@ -56,13 +64,23 @@ module Sockeye
     # then attempt to push the payload to each of them
     #
     def deliver_to_many(payload:, identifiers:)
+      puts "deliver_to_many"
       identifiers.each do |identifier|
+        puts "delivering to identifier:"
+        puts identifier
         identified_connections = connections[identifier]
+        puts "identified_connections"
+        puts identified_connections
         next unless identified_connections.is_a? Array
         identified_connections.each do |connection|
+          puts "connection"
+          puts connection
           begin
+            puts "sending"
             connection.send({payload: payload, status: payload.dig(:status) || 200}.to_json, :type => :text)
+            puts "sent"
           rescue
+            puts "something blew up"
           end
         end
       end
@@ -73,6 +91,7 @@ module Sockeye
     # and delivery actions for clients and pushers.
     #
     def listen
+      puts "listen..."
       EM.run do
         WebSocket::EventMachine::Server.start(host: self.host, port: self.port) do |ws|
 
@@ -80,6 +99,7 @@ module Sockeye
           #
           ws.onmessage do |message, type|
 
+            puts "onmessage"
             # Attempt to parse the received data as JSON
             #
             message_json = json_try_parse(message)
@@ -109,11 +129,15 @@ module Sockeye
               # then push out the payload to all connected specified clients
               #
               when :deliver
+                puts "deliver"
+                puts message_json
                 if message_json[:secret_token] == self.secret_token
+                  puts "good token"
                   deliver_to_many(payload: message_json[:payload], identifiers: [message_json[:identifiers]].flatten)
                   ws.send({payload: "payload pushed", status: 201}.to_json, :type => :text)
                   ws.close
                 else
+                  puts "bad token"
                   ws.send({payload: "authentication failure", status: 401}.to_json, :type => :text)
                   ws.close
                 end
